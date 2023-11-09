b5ec85dfdf9c1f08ae01828461f4d7e1eb549d52
BraedenChoo <braeden.choo@mail.utoronto.ca>
2023-11-08 23:17:44 -0500

    getters and setters created for project entity

--- src/entity/Project.java
+++ src/entity/Project.java
@@ -1,2 +1,32 @@
-package entity;public class Project {
+package entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Project {
+    private final int id;
+    private String name;
+    private LocalDate deadline;
+    private ArrayList<Task> tasks;
+
+    public Project(int id, String name, LocalDate deadline) {
+        this.id = id;
+        this.name = name;
+        this.deadline = deadline;
+        tasks = new ArrayList<Task>();
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+    public String getName() {
+        return name;
+    }
 }
