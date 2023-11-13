df4fba097044ec2ae4cb35f8187d81349a09d5e3
William Chang Liu <96327541+wojiaotreaty@users.noreply.github.com>
2023-11-12 21:51:53 -0500

    Fixed variable naming and typing in the Interactor and DataAccessInterface to correctly represent a list of tasks. Additionally added a docstring to the DataAccessInterface to make it implementation clearer.

--- src/use_case/add_project/AddProjectDataAccessInterface.java
+++ src/use_case/add_project/AddProjectDataAccessInterface.java
@@ -1,7 +1,16 @@
 package use_case.add_project;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public interface AddProjectDataAccessInterface {
-    void saveTasks(HashMap<String, String> taskMap);
+    /**
+     * Takes an ArrayList of tasks to put into the calendar. Each task is represented by a HashMap, mapping
+     * an attribute of the task to its value. Each task will have the following keys and values:
+     *      - TaskName: <name of the task>
+     *      - TaskDescription: <description of the task>
+     *      - TaskDeadline: <deadline of the task>
+     * @param taskMap
+     */
+    void saveTasks(ArrayList<HashMap<String, String>> taskMap);
 }
