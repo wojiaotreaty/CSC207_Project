5e4c822bc2b771601de664ffb95280b7fab0d154
Hamin Lee <daniehm0503@gmail.com>
2023-11-23 22:33:15 -0500

    created files and completed files for most files for login use case, and added multiple files to interact with the User object

--- src/use_case/login/LoginDataAccessInterface.java
+++ src/use_case/login/LoginDataAccessInterface.java
@@ -1,4 +1,11 @@
 package use_case.login;
 
+import entity.CommonUser;
+
 public interface LoginDataAccessInterface {
+    boolean existsByName(String identifier);
+
+    void save(CommonUser user);
+
+    CommonUser get(String username);
 }
