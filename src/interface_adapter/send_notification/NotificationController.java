9763d9f14064cac0a23682ca1e141c957354847d
BraedenChoo <braeden.choo@mail.utoronto.ca>
2023-11-24 15:50:32 -0500

    Notification controller and input data completed

--- src/interface_adapter/send_notification/NotificationController.java
+++ src/interface_adapter/send_notification/NotificationController.java
@@ -1,2 +1,17 @@
-package interface_adapter.send_notification;public class NotificationController {
+package interface_adapter.send_notification;
+
+import use_case.send_notification.NotificationInputBoundary;
+import use_case.send_notification.NotificationInputData;
+
+import java.time.LocalDate;
+
+public class NotificationController {
+    final NotificationInputBoundary notificationInteractor;
+    public NotificationController(NotificationInputBoundary notificationInteractor) {
+        this.notificationInteractor = notificationInteractor;
+    }
+    public void execute(LocalDate date) {
+        NotificationInputData notificationInputData = new NotificationInputData(date);
+        notificationInteractor.execute(notificationInputData)
+    }
 }
--- src/use_case/send_notification/NotificationInputData.java
+++ src/use_case/send_notification/NotificationInputData.java
@@ -1,2 +1,15 @@
-package use_case.send_notification;public class NotificationInputData {
+package use_case.send_notification;
+
+import java.time.LocalDate;
+
+public class NotificationInputData {
+    private final LocalDate date;
+
+    public NotificationInputData(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
 }
