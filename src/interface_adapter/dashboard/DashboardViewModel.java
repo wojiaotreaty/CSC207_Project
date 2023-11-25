d46e1afc0bfba49f86297a2ef1d1723b82f79453
William Chang Liu <96327541+wojiaotreaty@users.noreply.github.com>
2023-11-24 20:32:59 -0500

    Implemented the DashboardViewModel class such that it extends the ViewModel abstract class

--- src/interface_adapter/dashboard/DashboardViewModel.java
+++ src/interface_adapter/dashboard/DashboardViewModel.java
@@ -1,4 +1,34 @@
 package interface_adapter.dashboard;
 
-public class DashboardViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class DashboardViewModel extends ViewModel {
+    public final String TITLE_LABEL = "Project Dashboard";
+
+    private DashboardState state = new DashboardState();
+
+    public DashboardViewModel() {
+        super("Project Dashboard");
+    }
+
+    public void setState(DashboardState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public DashboardState getState() {
+        return state;
+    }
 }
