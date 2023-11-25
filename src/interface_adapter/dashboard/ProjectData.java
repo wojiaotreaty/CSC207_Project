59593bca5371becb01bafaf7e9a18bc62ac5120a
William Chang Liu <96327541+wojiaotreaty@users.noreply.github.com>
2023-11-24 20:38:14 -0500

    Created the ProjectData data class to cleanly store all data of a project for front end use. Implemented getters for all these attributes.

--- src/interface_adapter/dashboard/ProjectData.java
+++ src/interface_adapter/dashboard/ProjectData.java
@@ -1,2 +1,28 @@
-package interface_adapter.dashboard;public class ProjectData {
+package interface_adapter.dashboard;
+
+public class ProjectData {
+    private String projectTitle = "";
+    private String projectID = "";
+    private String projectDescription = "";
+    private String projectTasks = "";
+
+    public ProjectData(String projectID, String projectTitle, String projectDescription, String projectTasks) {
+        this.projectTitle = projectTitle;
+        this.projectID = projectID;
+        this.projectDescription = projectDescription;
+        this.projectTasks = projectTasks;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+    public String getProjectTitle() {
+        return projectTitle;
+    }
+    public String getProjectID() {
+        return projectID;
+    }
+    public String getProjectTasks() {
+        return projectTasks;
+    }
 }
