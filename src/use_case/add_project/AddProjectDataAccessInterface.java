38b56ca2d6e0b3b8988cf9d9e68db9f3fb1edede
William Chang Liu <96327541+wojiaotreaty@users.noreply.github.com>
2023-11-25 00:18:28 -0500

    Removed saveTasks from the DAI. As the method of adding a project to the User entity has changed, the responsibility has been shifted to the Interactor to fetch the user and update its list of projects. New methods in the DAI now reflect the newly required methods.

--- src/use_case/add_project/AddProjectDataAccessInterface.java
+++ src/use_case/add_project/AddProjectDataAccessInterface.java
@@ -3,10 +3,7 @@ package use_case.add_project;
 import entity.Project;
 
 public interface AddProjectDataAccessInterface {
-    /**
-     * @param project - a project object containing all information related to the project
-     * given a Project object containing all details related to a given project, saveTasks will save the project into
-     * the calendar database
-     */
-    void saveTasks(Project project);
+    User getCurrentUser();
+    boolean saveUser(User user);
+    String generateNewProjectId();
 }
