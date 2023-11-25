1bfb961b016d8562d7f108d00d35e7efa017d2ed
William Chang Liu <96327541+wojiaotreaty@users.noreply.github.com>
2023-11-24 20:44:15 -0500

    Modified the DashboardView to use information given by the DashboardState. Additionally, the controller has been injected into the view such that when the Add Project button is pressed and fields are appropriately filled, the Add Project use case is triggered instead of the current placeholder actions.

--- src/view/DashboardView.java
+++ src/view/DashboardView.java
@@ -1,22 +1,36 @@
 package view;
 
-import interface_adapter.add_project.AddProjectState;
-import interface_adapter.add_project.AddProjectViewModel;
+import interface_adapter.dashboard.DashboardState;
+import interface_adapter.dashboard.DashboardViewModel;
+import interface_adapter.dashboard.ProjectData;
 import interface_adapter.add_project.AddProjectController;
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.text.ParseException;
 import java.util.ArrayList;
 import javax.swing.text.MaskFormatter;
 
-public class DashboardView extends JFrame {
+public class DashboardView extends JFrame implements PropertyChangeListener {
+    private final DashboardViewModel dashboardViewModel;
     private JPanel dashboardPanel;
-    private ArrayList<String> projectsList;
+    private ArrayList<ProjectData> projectsList;
 
-    public DashboardView() {
-        projectsList = new ArrayList<>();
+    public DashboardView(DashboardViewModel dashboardViewModel, AddProjectController addProjectController) {
+        this.dashboardViewModel = dashboardViewModel;
+
+        DashboardState dashboardState = dashboardViewModel.getState();
+
+        dashboardViewModel.addPropertyChangeListener(this);
+
+        projectsList = dashboardState.getProjects();
+
+        displayAllProjects();
 
         setTitle("Project Dashboard");
         setSize(600, 400);
@@ -38,7 +52,11 @@ public class DashboardView extends JFrame {
         addProjectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                showAddProjectPopup();
+                try {
+                    showAddProjectPopup();
+                } catch (ParseException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
 
@@ -48,8 +66,8 @@ public class DashboardView extends JFrame {
     }
 
     private void updateEmptyDashboardLabel() {
-        dashboardPanel.removeAll();
         if (projectsList.isEmpty()) {
+            dashboardPanel.removeAll();
             JLabel emptyLabel = new JLabel("Click Add Project to add a project");
             emptyLabel.setFont(new Font("Arial", Font.BOLD, 16));
             emptyLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -59,14 +77,21 @@ public class DashboardView extends JFrame {
             dashboardPanel.add(emptyLabel);
         } else {
             dashboardPanel.setLayout(new BoxLayout(dashboardPanel, BoxLayout.Y_AXIS));
-            projectsList.forEach(project -> {
-                ProjectPanel projectPanel = new ProjectPanel(project, "Description Placeholder");
-                dashboardPanel.add(projectPanel);
-                dashboardPanel.add(Box.createVerticalStrut(10));
-            });
         }
         dashboardPanel.revalidate();
         dashboardPanel.repaint();
+    }
+
+    private void displayAllProjects() {
+        dashboardPanel.removeAll();
+
+        for (ProjectData project : projectsList) {
+            ProjectPanel projectPanel = new ProjectPanel(project);
+            dashboardPanel.add(projectPanel);
+            dashboardPanel.add(Box.createVerticalStrut(10));
+            dashboardPanel.revalidate();
+            updateEmptyDashboardLabel();
+        }
     }
 
     private void showAddProjectPopup() throws ParseException {
@@ -96,13 +121,12 @@ public class DashboardView extends JFrame {
             if (!projectName.isEmpty()) {
                 if (!projectDescription.isEmpty()) {
                     if (deadline.matches("\\d{4}/\\d{2}/\\d{2}")) {
-                        projectsList.add(projectName);
 
-                        ProjectPanel projectPanel = new ProjectPanel(projectName, projectDescription);
-                        dashboardPanel.add(projectPanel);
-                        dashboardPanel.add(Box.createVerticalStrut(10));
-                        dashboardPanel.revalidate();
-                        updateEmptyDashboardLabel();
+                        addProjectController.execute(
+                                projectName,
+                                projectDescription,
+                                deadline
+                        );
 
                         popupFrame.dispose();
                     } else {
@@ -130,14 +154,12 @@ public class DashboardView extends JFrame {
     }
 
     private class ProjectPanel extends JPanel {
-        private String projectName;
-        private String projectDescription;
+        private final ProjectData projectData;
         private static final int PANEL_MIN_MAX_HEIGHT = 100;
         private static final int ARC_SIZE = 20;
 
-        public ProjectPanel(String projectName, String projectDescription) {
-            this.projectName = projectName;
-            this.projectDescription = projectDescription;
+        public ProjectPanel(ProjectData projectData) {
+            this.projectData = projectData;
             setMinimumSize(new Dimension((int) (DashboardView.this.getWidth() * 0.95), PANEL_MIN_MAX_HEIGHT));
             setMaximumSize(new Dimension((int) (DashboardView.this.getWidth() * 0.95), PANEL_MIN_MAX_HEIGHT));
             setPreferredSize(new Dimension((int) (DashboardView.this.getWidth() * 0.95), PANEL_MIN_MAX_HEIGHT));
@@ -145,7 +167,7 @@ public class DashboardView extends JFrame {
 
             addMouseListener(new java.awt.event.MouseAdapter() {
                 public void mouseClicked(java.awt.event.MouseEvent evt) {
-                    JOptionPane.showMessageDialog(null, "Clicked on project: " + projectName);
+                    JOptionPane.showMessageDialog(null, "Clicked on project: " + projectID);
                 }
             });
         }
@@ -159,12 +181,17 @@ public class DashboardView extends JFrame {
             g2d.setColor(Color.BLACK);
             g2d.setFont(new Font("Arial", Font.BOLD, 16));
             FontMetrics metrics = g2d.getFontMetrics();
-            int textWidth = metrics.stringWidth(projectName);
+            int textWidth = metrics.stringWidth(projectData.getProjectTitle());
             int x = (getWidth() - textWidth) / 2;
             int y = getHeight() / 2 + metrics.getHeight() / 4;
-            g2d.drawString(projectName, x, y);
+            g2d.drawString(projectData.getProjectTitle(), x, y);
             g2d.dispose();
         }
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+        DashboardState state = (DashboardState) evt.getNewValue();
+        displayAllProjects();
     }
 
     public static void main(String[] args) {
