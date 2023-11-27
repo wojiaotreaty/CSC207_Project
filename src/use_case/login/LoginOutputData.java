54407729e19f444ef0d7031956777557ed51f400
Hamin Lee <daniehm0503@gmail.com>
2023-11-27 14:14:14 -0500

    added docstring that explains the projects datatype

--- src/use_case/login/LoginOutputData.java
+++ src/use_case/login/LoginOutputData.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class LoginOutputData {
 
     private final String username;
+    /*
+    projects is an arraylist of arraylist where each inner arraylist represents a project
+    and it contains a string representation of all tasks related to that project
+
+    projects is unique to every user.
+     */
     private final ArrayList<ArrayList<String>> projects;
     private boolean useCaseFailed;
 
