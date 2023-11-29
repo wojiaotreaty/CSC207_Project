755ea8ef8bd96171b1cb86bfd83813bdb1b62d12
VedantTheGreat <vedantswamy2004@gmail.com>
2023-11-29 15:41:04 -0500

    Changed the view completely in order for it to look more presentable

--- src/view/ProjectView.java
+++ src/view/ProjectView.java
@@ -25,24 +25,59 @@ public class ProjectView extends JFrame implements PropertyChangeListener {
         JFrame popupFrame = new JFrame("Project");
         popupFrame.setSize(400, 250);
 
-        JPanel popupPanel = new JPanel(new GridLayout(4, 2));
+        JPanel popupPanel = new JPanel(new BoxLayout(popupPanel, BoxLayout.Y_AXIS));
         // The project title
-        JTextField Title = new JTextField(project.getProjectName());
-        popupFrame.add(Title);
+        JLabel Title=new JLabel("Project Name");
+        JTextField title = new JTextField(project.getProjectName());
+        title.setEditable(false);
+        popupPanel.add(Title);
+        popupPanel.add(title);
         // The project description
+        JLabel description=new JLabel("Project Description");
         JTextArea textArea = new JTextArea(5, 20);
         JScrollPane scrollPane = new JScrollPane(popupPanel);
         textArea.setEditable(false);
         textArea.append(project.getProjectDescritpion() + "\n");
+        // Creating spacing for the rest of the popup panel
+        textArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        popupPanel.add(textArea);
+        popupPanel.add(description);
         // The list of tasks along with their deadlines and descriptions and status
-        JCheckBox status = null;
+
         for (String task : project.getTasks()) {
             String[] arrOfStr = task.split("|uwu|");
 
             String taskName = arrOfStr[0];
             String taskDescription = arrOfStr[1];
             String taskDeadline = arrOfStr[2];
-            String[] taskStatus = {arrOfStr[3]};
+            String taskStatus = arrOfStr[3];
+            JLabel tName = new JLabel("Task Name");
+            JTextField name=new JTextField(taskName);
+            name.setEditable(false);
+            // Adding the taskName label and the taskName textField into one panel
+            JPanel panel1= new JPanel(new GridLayout(2,1));
+            panel1.add(tName);
+            panel1.add(name);
+            JLabel tDeadline= new JLabel("Task Deadline");
+            JTextField deadline =new JTextField(taskDeadline);
+            deadline.setEditable(false);
+            // Adding the deadline label and the deadline text field into one panel
+            JPanel panel2= new JPanel(new GridLayout(2,1));
+            panel1.add(tDeadline);
+            panel1.add(deadline);
+            // Create a check box which is always checked if the task Status is 1
+            if (taskStatus.equals("true")){
+                JCheckBox status = new JCheckBox("status",true);
+            }
+            else{
+                JCheckBox status = new JCheckBox("status",true);
+            }
+
+            JPanel labelsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            labelsPanel.add(label1);
+            labelsPanel.add(label2);
+
+            mainPanel.add(labelsPanel, BorderLayout.SOUTH);
             JTextArea taskdescription = new JTextArea(5, 20);
             textArea.setEditable(false);
             textArea.append(taskName + "       " + "deadline:" + taskDeadline + "\n" + taskDescription + "\n");
