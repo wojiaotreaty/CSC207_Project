29bf3c9e6d813ab9b03d21e94767babb43f90049
VedantTheGreat <vedantswamy2004@gmail.com>
2023-11-30 12:17:55 -0500

    updated the data access interface

--- src/use_case/RefactorProject/RefactorProjectDataAccessInterface.java
+++ src/use_case/RefactorProject/RefactorProjectDataAccessInterface.java
@@ -1,4 +1,11 @@
-package RefactorProject;
+package use_case.RefactorProject;
+
+import entity.Project;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public interface RefactorProjectDataAccessInterface {
+    ArrayList<Project> getProjects(String[] Ids);
+    void setTaskDeadline(String projectId, String taskId, LocalDate deadline);
 }
