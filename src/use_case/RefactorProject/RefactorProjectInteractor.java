a4ba06db38eb4548ddcf3ae611d156a40e9ba9c5
VedantTheGreat <vedantswamy2004@gmail.com>
2023-11-30 06:47:45 -0500

    updated the interactor so that I use the task Entity and the DAO

--- src/use_case/RefactorProject/RefactorProjectInteractor.java
+++ src/use_case/RefactorProject/RefactorProjectInteractor.java
@@ -1,19 +1,19 @@
 package use_case.RefactorProject;
-    import entity.Project;
+import entity.Project;
 import entity.ProjectFactory;
-
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-    import java.text.ParseException;
-    import java.text.SimpleDateFormat;
-    import java.time.LocalDate;
-    import java.time.LocalDateTime;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-    import java.util.Date;
-    import java.util.HashMap;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Objects;
 
     public class RefactorProjectInteractor implements RefactorProjectInputBoundary {
@@ -30,37 +30,34 @@ import java.util.Objects;
 
         @Override
         public void execute(RefactorProjectInputData refactorProjectInputData) {
-            Date now = new Date();
-            Project Project = refactorProjectInputData.getProject();
-            ArrayList<HashMap<String, String>> tasks = refactorProjectInputData.getTasks();
+            // Getting the time right now
+            LocalDateTime now = LocalDateTime.now();
+            int projectID = refactorProjectInputData.getProjectID();
+            // getting the project from the DAO
+            String [] l= {Integer.toString(projectID)};
+            Project project =  getProjects(l)[0];
+            ArrayList<Task> tasks = project.getTasks();
             int taskIndex = 0;
-            while (tasks.get(taskIndex).get("Done") == "1") {
-                taskIndex++;
+
+            while (tasks.get(taskIndex).getStatus == true) {
+                taskIndex=taskIndex+1;
             }
             //getting the task which was completed most recently just before the deadline
-            HashMap<String, String> completedTask = tasks.get(taskIndex - 1);
+           Task completedTask = tasks.get(taskIndex - 1);
             // getting the deadline
-            String deadline = completedTask.get("TaskDeadline");
+            LocalDate deadline = completedTask.getDeadline();
             // assuming the TasksDeadline is in "YYYY-MM-DD" format
-            SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD");
-            Date deadlineDate = null;
-            try {
-                deadlineDate = formatter.parse(deadline);
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
             // claculating the time difference between the current date and the deadline date
-            long timeDifference = deadlineDate.getTime() - now.getTime();
+            long timeDifference = deadline.until(now, ChronoUnit.MILLIS)
             double deciDays = timeDifference / 86400000;
             long days = Math.round(deciDays);
             ArrayList<LocalDate> dates = new ArrayList<>();
             for (int i = taskIndex; i < tasks.size(); i++) {
-                String taskDeadline=tasks.get(i).get("TaskDeadline");
-                LocalDate date= LocalDate.parse(taskDeadline);
-                 LocalDate shiftedTaskDeadline=date.minusDays(days);
-                 String shiftedTaskDeadlineString=shiftedTaskDeadline.toString();
-                 tasks.get(i).put("TaskDeadline",shiftedTaskDeadlineString);
+                LocalDate taskDeadline = tasks.get(i).getDeadline();
+                LocalDate shiftedTaskDeadline = taskDeadline.minusDays(days);
+                tasks.get(i).setDeadline(shiftedTaskDeadline);
             }
+            // TODO:Update the project entities
 
         }
     }
