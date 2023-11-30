e48d162417c0f463affb46d3577344524ae5b474
VedantTheGreat <vedantswamy2004@gmail.com>
2023-11-30 12:19:31 -0500

    pushing  the output data

--- src/use_case/RefactorProject/RefactorProjectOutputData.java
+++ src/use_case/RefactorProject/RefactorProjectOutputData.java
@@ -1,4 +1,20 @@
-package RefactorProject;
+package use_case.RefactorProject;
+
+import java.util.ArrayList;
 
 public class RefactorProjectOutputData {
+    private ArrayList<String> tasks;
+
+
+    public RefactorProjectOutputData(ArrayList<String> tasks) {
+        this.tasks=tasks;
+    }
+
+    public ArrayList<String> getTask() {
+        return tasks;
+    }
+
+    public void setTask(ArrayList<String> tasks) {
+        this.tasks = tasks;
+    }
 }
