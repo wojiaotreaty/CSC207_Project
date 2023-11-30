f9bbbb63a290b5d9e7fd92b908b4abb4f1e13148
BraedenChoo <braeden.choo@mail.utoronto.ca>
2023-11-30 03:33:27 -0500

    Implemented interactor and DAO interface.

--- src/use_case/set_status/SetStatusInteractor.java
+++ src/use_case/set_status/SetStatusInteractor.java
@@ -1,4 +1,13 @@
 package use_case.set_status;
 
-public class SetStatusInteractor {
+public class SetStatusInteractor implements SetStatusInputBoundary {
+    final SetStatusProjectsDataAccessInterface projectsDataAccessObject;
+    public SetStatusInteractor(SetStatusProjectsDataAccessInterface projectsDataAccessObject) {
+        this.projectsDataAccessObject = projectsDataAccessObject;
+    }
+
+    @Override
+    public void execute(SetStatusInputData setStatusInputData) {
+        projectsDataAccessObject.setStatus(setStatusInputData.getProjectId(), setStatusInputData.getTaskId());
+    }
 }
---
+++ src/use_case/set_status/SetStatusProjectsDataAccessInterface.java
@@ -0,0 +1,5 @@
+package use_case.set_status;
+
+public interface SetStatusProjectsDataAccessInterface {
+    void setStatus(String projectId, String taskId);
+}
