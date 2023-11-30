e62374cb0b091a5b524934694ff43466a2f4ecb2
BraedenChoo <braeden.choo@mail.utoronto.ca>
2023-11-30 15:41:38 -0500

    fixed CommonProjectFactory and CommonUserFactory.

--- src/entity/CommonProjectFactory.java
+++ src/entity/CommonProjectFactory.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 
 public class CommonProjectFactory implements ProjectFactory {
     Project create(String projectId, String projectName, String projectDesc, ArrayList<Task> tasks) {
-        return new Project(projectId, projectName, projectDesc, tasks);
+        return new CommonProject(projectId, projectName, projectDesc, tasks);
     }
 }
--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class CommonUserFactory implements UserFactory {
     User create(String username, String password, ArrayList<Project> projects) {
-        return new User(username, password, projects);
+        return new CommonUser(username, password, projects);
+    }
+    User create(String username, String password) {
+        return new CommonUser(username, password, projects);
     }
 }
