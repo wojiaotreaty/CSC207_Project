f0f853f3b9f02c77ae500d127b040edc45681294
William Chang Liu <96327541+wojiaotreaty@users.noreply.github.com>
2023-12-01 18:49:42 -0500

    Implemented the logic for ProjectData to parse the tasksString given into an ArrayList of tasks, each represented by an ArrayList of Strings.

--- test/use_case/add_project/AddProjectInteractorUnitTest.java
+++ test/use_case/add_project/AddProjectInteractorUnitTest.java
@@ -2,11 +2,8 @@ package use_case.add_project;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import entity.Project;
-import entity.User;
+import entity.*;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 
 public class AddProjectInteractorUnitTest {
 
@@ -25,14 +22,14 @@ public class AddProjectInteractorUnitTest {
         AddProjectOutputBoundary successPresenter = new AddProjectOutputBoundary() {
             @Override
             public void prepareSuccessView(AddProjectOutputData addProjectOutputData) {
-                assertEquals("100", addProjectOutputData.getProjectID());
+                assertEquals("100", addProjectOutputData.getProject().get(0));
+                assertEquals("CSC207 Group Project", addProjectOutputData.getProject().get(1));
+                assertEquals("Create an application, which makes use of Clean Architecture and SOLID Design principles, " +
+                        "which serves as an aid for time management and organizing deadlines. Upon adding a project, the" +
+                        "application will split the project into smaller deadlines which help the user to stay on track.",
+                        addProjectOutputData.getProject().get(2));
+                assertNotNull(addProjectOutputData.getProject().get(3));
             }
-//            AddProjectOutputData addProjectOutputData = new AddProjectOutputData(
-//                    projectID,
-//                    addProjectInputData.getProjectTitle(),
-//                    addProjectInputData.getProjectDetails(),
-//                    String.valueOf(tasksString)
-//            );
 
             @Override
             public void prepareFailView(String error) {
@@ -40,66 +37,20 @@ public class AddProjectInteractorUnitTest {
             }
         };
 
-        SignupInputBoundary interactor = new SignupInteractor(userRepository, successPresenter, new CommonUserFactory());
-        interactor.execute(inputData);
+        AddProjectInputBoundary addProjectInteractor = new AddProjectInteractor(
+                dummyUsersDataAccessInterface,
+                successPresenter,
+                new CommonProjectFactory(),
+                new CommonTaskFactory()
+        );
+        addProjectInteractor.execute(inputData);
     }
 
-    @Test
-    void failurePasswordMismatchTest() {
-        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
-        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
-
-        // This creates a presenter that tests whether the test case is as we expect.
-        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
-            @Override
-            public void prepareSuccessView(SignupOutputData user) {
-                // 2 things to check: the output data is correct, and the user has been created in the DAO.
-                fail("Use case success is unexpected.");
-            }
-
-            @Override
-            public void prepareFailView(String error) {
-                assertEquals("Passwords don't match.", error);
-            }
-        };
-
-        SignupInputBoundary interactor = new SignupInteractor(userRepository, failurePresenter, new CommonUserFactory());
-        interactor.execute(inputData);
-    }
-
-    @Test
-    void failureUserExistsTest() {
-        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
-        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
-
-        // Add Paul to the repo so that when we check later they already exist
-        UserFactory factory = new CommonUserFactory();
-        User user = factory.create("Paul", "pwd",  LocalDateTime.now());
-        userRepository.save(user);
-
-        // This creates a presenter that tests whether the test case is as we expect.
-        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
-            @Override
-            public void prepareSuccessView(SignupOutputData user) {
-                // 2 things to check: the output data is correct, and the user has been created in the DAO.
-                fail("Use case success is unexpected.");
-            }
-
-            @Override
-            public void prepareFailView(String error) {
-                assertEquals("User already exists.", error);
-            }
-        };
-
-        SignupInputBoundary interactor = new SignupInteractor(userRepository, failurePresenter, new CommonUserFactory());
-        interactor.execute(inputData);
-    }
-
-    private class DummyUsersDataAccessObject implements AddProjectDataAccessInterface {
+    private static class DummyUsersDataAccessObject implements AddProjectDataAccessInterface {
 
         @Override
         public User getUser(String username) {
-            return new DummyUser();
+            return new CommonUser("foobar", "baz");
         }
 
         @Override
@@ -113,8 +64,4 @@ public class AddProjectInteractorUnitTest {
         }
     }
 
-    private class DummyUser {
-        public void addProject(Project project) {
-        }
-    }
 }
