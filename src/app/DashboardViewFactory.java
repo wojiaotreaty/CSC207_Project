05bb81dc63c7f30108aad8fa7c357cc0108bda88
BraedenChoo <braeden.choo@mail.utoronto.ca>
2023-11-30 20:33:54 -0500

    Fixed DashboardViewFactory to include NotificationController.

--- src/app/DashboardViewFactory.java
+++ src/app/DashboardViewFactory.java
@@ -1,7 +1,15 @@
 package app;
 
+import entity.CommonProjectFactory;
 import interface_adapter.dashboard.DashboardViewModel;
+import interface_adapter.send_notification.NotificationController;
+import interface_adapter.send_notification.NotificationPresenter;
+import use_case.send_notification.NotificationInputBoundary;
+import use_case.send_notification.NotificationInteractor;
+import use_case.send_notification.NotificationOutputBoundary;
+import use_case.send_notification.NotificationUsersDataAccessInterface;
 import view.DashboardView;
+import entity.ProjectFactory;
 
 import javax.swing.*;
 import java.io.IOException;
@@ -11,11 +19,12 @@ public class DashboardViewFactory {
 
     public static DashboardView create(DashboardViewModel dashboardViewModel,
                                        AddProjectDataAccessInterface addProjectDataAccessInterface,
-                                       AddProjectOutputBoundary addProjectOutputBoundary) {
+                                       NotificationUsersDataAccessInterface notificationUsersDataAccessInterface) {
 
         try {
-            AddProjectController addProjectController = createAddProjectUseCase(dashboardViewModel, addProjectDataAccessInterface, addProjectOutputBoundary);
-            return new DashboardView(dashboardViewModel, addProjectController);
+            AddProjectController addProjectController = createAddProjectUseCase(dashboardViewModel, addProjectDataAccessInterface);
+            NotificationController notificationController = createNotificationUseCase(dashboardViewModel, notificationUsersDataAccessInterface);
+            return new DashboardView(dashboardViewModel, addProjectController, notificationController);
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Could not instantiate DashboardView.");
         }
@@ -24,17 +33,24 @@ public class DashboardViewFactory {
     }
 
     private static AddProjectController createAddProjectUseCase(DashboardViewModel dashboardViewModel,
-                                                            AddProjectDataAccessInterface addProjectDataAccessInterface,
-                                                            AddProjectOutputBoundary addProjectOutputBoundary) throws IOException {
+                                                            AddProjectDataAccessInterface addProjectDataAccessInterface) throws IOException {
 
         // Notice how we pass this method's parameters to the Presenter.
         AddProjectOutputBoundary addProjectOutputBoundary = new AddProjectPresenter(dashboardViewModel);
 
-        ProjectFactory projectFactory = new ProjectFactory();
+        ProjectFactory projectFactory = new CommonProjectFactory();
 
         AddProjectInputBoundary addProjectInteractor = new AddProjectInteractor(
                 addProjectDataAccessInterface, addProjectOutputBoundary, projectFactory);
 
         return new AddProjectController(addProjectInteractor);
+    }
+    private static NotificationController createNotificationUseCase(DashboardViewModel dashboardViewModel,
+                                                                    NotificationUsersDataAccessInterface notificationUsersDataAccessInterface) throws IOException {
+        NotificationOutputBoundary notificationOutputBoundary = new NotificationPresenter(dashboardViewModel);
+
+        NotificationInputBoundary notificationInteractor = new NotificationInteractor(notificationUsersDataAccessInterface, notificationOutputBoundary);
+
+        return new NotificationController(notificationInteractor);
     }
 }
