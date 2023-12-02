990e5d333f592fb9e71a75484668039403590aba
William Chang Liu <96327541+wojiaotreaty@users.noreply.github.com>
2023-12-02 16:04:57 -0500

    Added helper methods and test to check if both Add Project buttons are present. The first initially on the DashboardView, and the second present on the popup which appears after clicking the first Add Project button.

--- test/use_case/add_project/AddProjectEndToEndTest.java
+++ test/use_case/add_project/AddProjectEndToEndTest.java
@@ -1,9 +1,22 @@
 package use_case.add_project;
 
+import app.DashboardViewFactory;
+import app.LoginViewFactory;
 import app.Main;
-import entity.CommonUserFactory;
-import entity.UserFactory;
+import app.SignupViewFactory;
+import data_access.ProjectsDataAccessObject;
+import data_access.UsersDataAccessObject;
+import entity.*;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.dashboard.DashboardViewModel;
+import interface_adapter.delete_project.DeleteProjectViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupViewModel;
+import org.junit.jupiter.api.Test;
+import view.DashboardView;
+import view.LoginView;
 import view.SignupView;
+import view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,28 +29,67 @@ import java.time.LocalDateTime;
 
 import static org.junit.Assert.assertNotNull;
 
-class AddProjectEndToEndTest {
+public class AddProjectEndToEndTest {
 
     static String message = "";
     static boolean popUpDiscovered = false;
 
-    /**
-     * ensures there are at least 2 users in the CSV file for testing purposes
-     */
-    public void addTwoUsers() {
-        UserFactory uf = new CommonUserFactory();
-        FileUserDataAccessObject fudao;
+    public void goToDashboardView() throws IOException {
+        JFrame application = new JFrame("Login Example");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+        // The various View objects. Only one view is visible at a time.
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        // This keeps track of and manages which view is currently showing.
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        // For the Add Project End-to-End test, we can skip the login and signup views altogether
+        DashboardViewModel dashboardViewModel = new DashboardViewModel();
+        DeleteProjectViewModel deleteProjectViewModel = new DeleteProjectViewModel();
+
+        UserFactory userFactory = new CommonUserFactory();
+        ProjectFactory projectFactory = new CommonProjectFactory();
+        TaskFactory taskFactory = new CommonTaskFactory();
+
+        ProjectsDataAccessObject projectsDAO;
+        UsersDataAccessObject usersDataAccessObject = null;
         try {
-            fudao = new FileUserDataAccessObject("./users.csv", uf);
+            projectsDAO = new ProjectsDataAccessObject("./projects.csv",
+                    projectFactory, taskFactory);
+            usersDataAccessObject = new UsersDataAccessObject("./users.csv",
+                    userFactory, projectsDAO);
+
+            User user = new CommonUser("foobar", "baz");
+            usersDataAccessObject.saveUser(user);
+
+            dashboardViewModel.getState().setUsername("foobar");
+
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("ERROR: IOexception when creating UsersDAO");
         }
-        fudao.save(uf.create("user1", "password1", LocalDateTime.now()));
-        fudao.save(uf.create("user2", "password2", LocalDateTime.now()));
+
+        DashboardView dashboardView = DashboardViewFactory.create(dashboardViewModel, deleteProjectViewModel, viewManagerModel, usersDataAccessObject, usersDataAccessObject, usersDataAccessObject);
+        assert dashboardView != null;
+        views.add(dashboardView, dashboardView.viewName);
+
+        viewManagerModel.setActiveView(dashboardView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        application.pack();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.setSize(600, 400);
+        application.setLayout(new BorderLayout());
+        application.setResizable(false);
+        application.setVisible(true);
     }
 
 
-    public JButton getButton() {
+    public JButton getFirstAddProjectButton() {
         JFrame app = null;
         Window[] windows = Window.getWindows();
         for (Window window : windows) {
@@ -56,51 +108,94 @@ class AddProjectEndToEndTest {
 
         JPanel jp2 = (JPanel) jp.getComponent(0);
 
-        SignupView sv = (SignupView) jp2.getComponent(0);
+        DashboardView sv = (DashboardView) jp2.getComponent(0);
 
-        JPanel buttons = (JPanel) sv.getComponent(4);
+        JPanel buttons = (JPanel) sv.getComponent(0);
 
-        return (JButton) buttons.getComponent(2); // this should be the clear button
+        return (JButton) buttons.getComponent(1); // this should be the first add project button
+    }
+
+    public JButton getSecondAddProjectButton() {
+        Window[] windows = Window.getWindows();
+
+        for (Window window : windows) {
+
+            if (window instanceof JFrame jframe) {
+
+                // Ensures we are on the right window.
+                if (jframe.getTitle().equals("Add Project")) {
+                    Component root = jframe.getComponent(0);
+
+                    Component cp = ((JRootPane) root).getContentPane();
+
+                    JPanel jp = (JPanel) cp;
+
+                    JPanel jp2 = (JPanel) jp.getComponent(0);
+
+                    return (JButton) jp2.getComponent(7);  // this should be the second add project button
+                }
+            }
+        }
+
+        return null;
+
     }
 
     /**
      *
-     * Test that the Clear button is present and where it is expected to be
+     * Test that the first Add Project button is present and upon clicked, opens a second window where another
+     * Add Project button is tested to be present.
      */
-    @org.junit.Test
-    public void testClearButtonPresent() {
-        Main.main(null);
-        JButton button = getButton();
-        assert(button.getText().equals("Clear"));
+    @Test
+    public void testAddProjectButtonsPresent() throws IOException {
+        goToDashboardView();
+        JButton firstAddProjectButton = getFirstAddProjectButton();
+        assert(firstAddProjectButton.getText().equals("Add Project"));
+
+        firstAddProjectButton.doClick();
+        JButton secondAddProjectButton = getSecondAddProjectButton();
+        assert(secondAddProjectButton.getText().equals("Add Project"));
     }
 
     /**
      *
-     * Test that pressing the Clear button deletes all users. This test first
-     * adds two users to the CSV file, then starts the program, then clicks the Clear button,
-     * and then checks that the file's length has decreased.
+     * Test that pressing the second Add Project Button will all fields filled will call
+     * the add project use case interactor. This test first starts the program at the DashboardView,
+     * navigates to the window with the second Add Project button, fill all required fields,
+     * then clicks the Add Project button. Finally, checks that the project has visually been
+     * added to the DashboardView.
      */
-    @org.junit.Test
-    public void testClearUsersDeletedUsersFromFile() {
+    @Test
+    public void testAddProject() throws IOException {
+        goToDashboardView();
 
-        addTwoUsers();
-        Main.main(null);
-        JButton button = getButton();
+        JButton firstAddProjectButton = getFirstAddProjectButton();
+        firstAddProjectButton.doClick();
 
-        // since clicking the button should end up displaying a JDialog to the user to report the
-        // result, we set a timer, which will execute code necessary to complete the testing.
-        createCloseTimer().start();
+        Window[] windows = Window.getWindows();
 
-        button.doClick();
+        for (Window window : windows) {
 
-        // will continue execution here after the JDialog is closed
+            if (window instanceof JFrame jframe) {
 
-        // users.csv format
-        //username, password,timestamp (in format returned by a call like LocalDateTime.now())
-        //example
-        //user1,pass1,2023-10-12T14:46:26.733518
+                // Ensures we are on the right window.
+                if (jframe.getTitle().equals("Add Project")) {
+                    Component root = jframe.getComponent(0);
 
-        //check the users.csv file to ensure the users are gone
+                    Component cp = ((JRootPane) root).getContentPane();
+
+                    JPanel jp = (JPanel) cp;
+
+                    JPanel jp2 = (JPanel) jp.getComponent(0);
+
+                    JButton secondAddProjectButton = (JButton) jp2.getComponent(7); // this should be the second add project button
+                    secondAddProjectButton.doClick();
+                }
+            }
+        }
+
+
+
         try {
             int lines = countLines();
             System.out.println("lines left = " + lines);
@@ -111,98 +206,6 @@ class AddProjectEndToEndTest {
         }
 
 
-    }
-
-    /**
-     *
-     * This test is the same as above, but it additionally checks that the JDialog contains the names of
-     * all users deleted from the file.
-     */
-    @org.junit.Test
-    public void testClearUsersPopUpShown() {
-
-        addTwoUsers();
-        popUpDiscovered = false;
-
-        Main.main(null);
-        JFrame app = null;
-
-        JButton button = getButton();
-
-
-        // since clicking the button should end up displaying a JDialog to the user to report the
-        // result, we set a timer, which will execute code necessary to complete the testing.
-        createCloseTimer().start();
-
-        //click the button
-        button.doClick();
-
-        // will continue execution here after the JDialog is closed
-
-        // confirm a popUp was discovered
-        assert(popUpDiscovered);
-        System.out.println("popup was detected successfully.");
-
-    }
-
-    @org.junit.Test
-    public void testClearUsersReturnedUsersDeleted() throws InterruptedException {
-
-        addTwoUsers();
-        message = "";
-
-        Main.main(null);
-
-        JButton button = getButton();
-
-        // since clicking the button should end up displaying a JDialog to the user to report the
-        // result, we set a timer, which will execute code necessary to complete the testing.
-        createCloseTimer().start();
-
-        //click the button
-        button.doClick();
-
-        // will continue execution here after the JDialog is closed
-
-        // check the message
-        assert(message.contains("user1") && message.contains("user2"));
-    }
-
-    private Timer createCloseTimer() {
-        ActionListener close = new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                Window[] windows = Window.getWindows();
-                for (Window window : windows) {
-
-                    if (window instanceof JDialog) {
-
-                        JDialog dialog = (JDialog)window;
-
-                        // this ignores old dialogs
-                        if (dialog.isVisible()) {
-                            String s = ((JOptionPane) ((BorderLayout) dialog.getRootPane()
-                                    .getContentPane().getLayout()).getLayoutComponent(BorderLayout.CENTER)).getMessage().toString();
-                            System.out.println("message = " + s);
-
-                            // store the information we got from the JDialog
-                            ClearUsersTest.message = s;
-                            ClearUsersTest.popUpDiscovered = true;
-
-                            System.out.println("disposing of..." + window.getClass());
-                            window.dispose();
-                        }
-                    }
-                }
-            }
-
-        };
-
-        Timer t = new Timer(1000, close);
-        t.setRepeats(false);
-        return t;
     }
 
     private static int countLines() throws IOException {
