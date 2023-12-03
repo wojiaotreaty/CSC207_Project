11ba7186b252e981c135764b1b9032bf85edfe10
BraedenChoo <braeden.choo@mail.utoronto.ca>
2023-12-03 08:06:52 -0500

    Implemented SetStatusPresenter

--- src/use_case/set_status/SetStatusPresenter.java
+++
@@ -1,8 +0,0 @@
-package use_case.set_status;
-
-public class SetStatusPresenter implements SetStatusOutputBoundary{
-    @Override
-    public void prepareSuccessView(SetStatusOutputData setStatusOutputData) {
-
-    }
-}
