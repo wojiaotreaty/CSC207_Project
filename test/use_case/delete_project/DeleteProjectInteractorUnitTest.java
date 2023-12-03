091389a5d7ef0d09dfd00d89b0fa1d3b261b96d7
YBthree <144300516+bing-j@users.noreply.github.com>
2023-12-02 22:59:59 -0500

    interactor unit test implemented and passed

--- test/use_case/delete_project/DeleteProjectInteractorUnitTest.java
+++ test/use_case/delete_project/DeleteProjectInteractorUnitTest.java
@@ -1,78 +1,77 @@
 package use_case.delete_project;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import entity.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class AddProjectInteractorUnitTest {
+import static org.junit.Assert.*;
+
+public class DeleteProjectInteractorUnitTest {
+
+    UserFactory userFactory = new CommonUserFactory();
+    ProjectFactory projectFactory = new CommonProjectFactory();
+    static User testUser;
+
+    static final String USERNAME = "username1";
+    static final String PROJECT_ID = "32";
+    static final String PROJECT_NAME = "foobarProjName";
+
+    @Before
+    public void init(){
+        ArrayList<Task> taskList = new ArrayList<>();
+        TaskFactory taskFactory = new CommonTaskFactory();
+        taskList.add(taskFactory.create(
+                "foobarTaskName1", LocalDate.parse("2022-12-22"), "foobarTaskDesc1"));
+        taskList.add(taskFactory.create(
+                "foobarTaskName2", LocalDate.parse("2022-12-23"), "foobarTaskDesc2"));
+
+        ArrayList<Project> projectList = new ArrayList<>();
+        projectList.add(this.projectFactory.create(PROJECT_ID, PROJECT_NAME,
+                "foobarProjDesc", taskList));
+        testUser = this.userFactory.create(USERNAME, "password1", projectList);
+    }
 
     @Test
     public void successTest() {
-        UserFactory userFactory = new CommonUserFactory();
-        ProjectFactory projectFactory = new CommonProjectFactory();
 
-        DeleteProjectInputData inputData = new DeleteProjectInputData(
-                "username1",
-                "32");
+        DeleteProjectInputData inputData = new DeleteProjectInputData(USERNAME, PROJECT_ID);
 
         DeleteProjectDataAccessInterface dummyUsersDataAccessInterface = new DeleteProjectDataAccessInterface() {
-
             @Override
-            public User getUser(String userId) {
-                if (userId.equals("32")) {
-                    ArrayList<Project> projectList = new ArrayList<>();
-                    projectList.add(projectFactory.create("32", "foobarName",
-                            "foobarDesc", null));
-                    return userFactory.create("username1", "password1",
-                            projectList);
+            public User getUser(String username) {
+                if (username.equals(DeleteProjectInteractorUnitTest.USERNAME)) {
+                    return DeleteProjectInteractorUnitTest.testUser;
                 } else return null;
             }
 
             @Override
             public boolean saveUser(User user) {
-
-            }
-        }
-
-        AddProjectOutputBoundary successPresenter = new AddProjectOutputBoundary() {
-            @Override
-            public void prepareSuccessView(AddProjectOutputData addProjectOutputData) {
-                assertEquals("100", addProjectOutputData.getProject().get(0));
-                assertEquals("CSC207 Group Project", addProjectOutputData.getProject().get(1));
-                assertEquals("Create an application, which makes use of Clean Architecture and SOLID Design principles, " +
-                                "which serves as an aid for time management and organizing deadlines. Upon adding a project, the" +
-                                "application will split the project into smaller deadlines which help the user to stay on track.",
-                        addProjectOutputData.getProject().get(2));
-
-                try {
-                    ArrayList<ArrayList<String>> projectTasks = new ArrayList<>();
-
-                    String[] tasks = addProjectOutputData.getProject().get(3).split("[|]uwu[|]");
-                    for (String task : tasks) {
-                        String[] taskComponents = task.split("`");
-                        projectTasks.add((ArrayList<String>) Arrays.asList(taskComponents));
-                    }
-                } catch (Error e) {
-                    fail("Text completion generated did not match the given restrictions.");
-                }
-            }
-
-            @Override
-            public void prepareFailView(String error) {
-                fail("Use case failure is unexpected.");
+                return user.equals(DeleteProjectInteractorUnitTest.testUser);
             }
         };
 
-        AddProjectInputBoundary addProjectInteractor = new AddProjectInteractor(
-                dummyUsersDataAccessInterface,
-                successPresenter,
-                new CommonProjectFactory(),
-                new CommonTaskFactory()
-        );
-        addProjectInteractor.execute(inputData);
+        DeleteProjectOutputBoundary successPresenter = deleteProjectOutputData -> {
+            assertEquals(deleteProjectOutputData.getProjectId(), DeleteProjectInteractorUnitTest.PROJECT_ID);
+            assertEquals(deleteProjectOutputData.getProjectName(), DeleteProjectInteractorUnitTest.PROJECT_NAME);
+
+            User resultUser = dummyUsersDataAccessInterface.getUser(DeleteProjectInteractorUnitTest.USERNAME);
+            assertNotNull("ERROR: user not found.", resultUser);
+            for (Project project : resultUser.getProjects()){
+                assertNotEquals("ERROR: Project not deleted.",
+                        DeleteProjectInteractorUnitTest.PROJECT_ID, project.getProjectId());
+            }
+
+
+
+        };
+
+        DeleteProjectInputBoundary testDeleteProjectInteractor = new DeleteProjectInteractor(
+                dummyUsersDataAccessInterface, successPresenter);
+        testDeleteProjectInteractor.execute(inputData);
     }
 
 }
