9f08cf547352a296083d66c503166c3de49f3bbe
VedantTheGreat <vedantswamy2004@gmail.com>
2023-12-06 17:39:04 -0500

    changed the input data in order to help me make changes to certain files that I have

--- src/use_case/RefactorProject/RefactorProjectInputData.java
+++ src/use_case/RefactorProject/RefactorProjectInputData.java
@@ -1,10 +1,29 @@
 package use_case.RefactorProject;
 public class RefactorProjectInputData {
 
-String id;
+private String id;
+private String userName;
 
 
-    public RefactorProjectInputData(String id) {
+    public RefactorProjectInputData(String id,String userName) {
         this.id=id;
+        this.userName=userName;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
