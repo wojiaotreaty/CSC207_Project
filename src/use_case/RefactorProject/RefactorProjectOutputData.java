e934485fd4523d62a7dacba1b29cfda30edbcede
VedantTheGreat <vedantswamy2004@gmail.com>
2023-12-06 19:13:07 -0500

    updated the refactor project output data so that it can match with the interactor

--- src/use_case/RefactorProject/RefactorProjectOutputData.java
+++ src/use_case/RefactorProject/RefactorProjectOutputData.java
@@ -1,20 +1,38 @@
 package use_case.RefactorProject;
 
+import entity.Project;
+import entity.Task;
+
 import java.util.ArrayList;
 
 public class RefactorProjectOutputData {
-    private ArrayList<String> tasks;
+private String projectId;
+private String projectName;
+private String projectDescription;
+private String tasks;
 
 
-    public RefactorProjectOutputData(ArrayList<String> tasks) {
-        this.tasks=tasks;
+    public RefactorProjectOutputData(String projectID, String projectName, String projectDescription, String tasks) {
+    this.projectId=projectID;
+    this.projectName=projectName;
+    this.projectDescription=projectDescription;
+    this.tasks=tasks;
     }
 
-    public ArrayList<String> getTask() {
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public String getTasks() {
         return tasks;
-    }
-
-    public void setTask(ArrayList<String> tasks) {
-        this.tasks = tasks;
     }
 }
