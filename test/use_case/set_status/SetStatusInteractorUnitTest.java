1dcc24bcc5ef1ccda84443e796b321b127ba7cf3
BraedenChoo <braeden.choo@mail.utoronto.ca>
2023-12-06 21:17:23 -0500

    Created SetStatusInteractorFalseUnitTest

--- test/use_case/set_status/SetStatusInteractorUnitTest.java
+++ test/use_case/set_status/SetStatusInteractorUnitTest.java
@@ -33,6 +33,27 @@ public class SetStatusInteractorUnitTest {
         SetStatusInteractor.execute(inputData);
     }
 
+    @Test
+    void SetFalseTest() {
+        SetStatusInputData inputData = new SetStatusInputData("fee", "1", "task1`task1 desc`2024-01-01`true");
+
+        SetStatusUsersDataAccessInterface dummyUsersDataAccessInterface = new DummyUsersDataAccessObject();
+
+        SetStatusOutputBoundary successPresenter = new SetStatusOutputBoundary() {
+
+            @Override
+            public void prepareSuccessView(SetStatusOutputData setStatusOutputData) {
+                assertEquals("1", setStatusOutputData.getProjectId());
+                assertEquals("task1`task1 desc`2024-01-01`true", setStatusOutputData.getTaskString());
+            }
+        };
+
+        SetStatusInputBoundary SetStatusInteractor = new SetStatusInteractor(
+                dummyUsersDataAccessInterface,
+                successPresenter);
+        SetStatusInteractor.execute(inputData);
+    }
+
     // Dummy DAO created for the purpose of the Interactor Unit Test.
     private static class DummyUsersDataAccessObject implements SetStatusUsersDataAccessInterface {
 
@@ -40,13 +61,25 @@ public class SetStatusInteractorUnitTest {
         public User getUser(String username) {
             TaskFactory taskFactory = new CommonTaskFactory();
             Task task1 = taskFactory.create("task1", LocalDate.parse("2024-01-01"), "task1 desc");
-            ProjectFactory projectFactory = new CommonProjectFactory();
-            ArrayList<Task> tasks = new ArrayList<>();
-            tasks.add(task1);
-            Project project1 = projectFactory.create("1", "foo", "project1 desc", tasks);
-            ArrayList<Project> projects = new ArrayList<>();
-            projects.add(project1);
-            return new CommonUser("foo", "baz", projects);
+            if (username.equals("foo")) {
+                ProjectFactory projectFactory = new CommonProjectFactory();
+                ArrayList<Task> tasks = new ArrayList<>();
+                tasks.add(task1);
+                Project project1 = projectFactory.create("1", "project1", "project1 desc", tasks);
+                ArrayList<Project> projects = new ArrayList<>();
+                projects.add(project1);
+                return new CommonUser("foo", "baz", projects);
+            }
+            else {
+                task1.setStatus(true);
+                ProjectFactory projectFactory = new CommonProjectFactory();
+                ArrayList<Task> tasks = new ArrayList<>();
+                tasks.add(task1);
+                Project project1 = projectFactory.create("1", "project1", "project1 desc", tasks);
+                ArrayList<Project> projects = new ArrayList<>();
+                projects.add(project1);
+                return new CommonUser("fee", "baz", projects);
+            }
         }
 
         @Override
